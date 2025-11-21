c5a18cf59cadcb2253b912b36c75292630d4f61d
Kieran Llarena <baymax709@gmail.com>
2025-11-21 17:50:57 -0500

    revert: remove --local tag

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ttyd --writable -t titleFixed="krayon.dev" -p 7681 sh -c "./portfolio-v2 --local; ./welcome.sh; exec bash"
+ttyd --writable -t titleFixed="krayon.dev" -p 7681 sh -c "./portfolio-v2; ./welcome.sh; exec bash"
